3d55d700ab7044183f7c6fc7940e423281db3f5d
Tyler <radabaughtyler24@gmail.com>
2018-01-11 15:59:52 -0800

    Step 2 number of amandas

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ import sys
 import csv
 import operator
 
+FIELD_NAMES = ['Transaction_date', 'Product', 'Price', 'Payment_Type', 'City', 'State', 'Country', 'Name', 'Account_Created', 'Last_Login', 'Latitude', 'Longitude']
+
 if len(sys.argv) != 2:
 	print "Useage: ./main.py your_csv_file.csv"
 	exit(1)
 
 SORT_BY_COLUMN_NUMBER = 1
 csv_file_name = sys.argv[1]
+
 
 # sorts csv data by given column number
 def sort_data():
@@ -22,13 +25,21 @@ def sort_data():
 		for row in sorted_csv:
 			print ', '.join(row)
 
+
 # prints a count of the number of customers named "Amanda"
 def count_amandas():
 	number_of_amandas = 0
 
-	# TODO add logic
+	with open(csv_file_name, 'r') as csv_file:
+		reader = csv.reader(csv_file, delimiter = ',')
+		for row in reader:
+			for field in row:
+				if isinstance(field, basestring):
+					if "amanda" in field.lower():
+						number_of_amandas += 1
 
-	print ("The file contains %(number_of_amandas)s customers named Amanda." % locals())
+
+	print ("The file contains %(number_of_amandas)i customers named Amanda." % locals())
 
 
 sort_data()
